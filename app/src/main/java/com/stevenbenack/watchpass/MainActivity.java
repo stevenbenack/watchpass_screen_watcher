b69a669d09fecafb48d8848006caa29ca6720071
Steven Benack <stevenbenack@users.noreply.github.com>
2019-04-21 21:00:03 -0500

    Main activity implements alert dialog to allow accessibility. Can now see typing in declared apps

--- app/src/main/java/com/stevenbenack/watchpass/MainActivity.java
+++ app/src/main/java/com/stevenbenack/watchpass/MainActivity.java
@@ -1,33 +1,70 @@
 package com.stevenbenack.watchpass;
 
+import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ServiceInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.accessibility.AccessibilityManager;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.Unbinder;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
     @BindView(R.id.password_field)
     EditText passwordField;
     @BindView(R.id.test_button)
     Button testButton;
 
-    private Unbinder unbinder;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        unbinder = ButterKnife.bind(this);
+        ButterKnife.bind(this);
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        unbinder.unbind();
+    protected void onStart() {
+        super.onStart();
+
+        if(!isAccessibilityServicePermissions(this, WatcherService.class)) {
+            // ask user to allow permission
+            Log.d(TAG, "Does not have Watcher Accessibility Service Enabled");
+        }
+
+        AlertDialog.Builder permissionDialog = new AlertDialog.Builder(this);
+        permissionDialog.setTitle(R.string.dialog_title);
+        permissionDialog.setMessage(R.string.dialog_text);
+        permissionDialog.setPositiveButton(R.string.dialog_affirmative_button_text, (dialog, which) -> {
+            Intent intent = new Intent(android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS);
+            startActivityForResult(intent, 0);
+        });
+        permissionDialog.setNegativeButton(R.string.dialog_cancel_button_text, (dialog, which) -> dialog.dismiss());
+        permissionDialog.show();
     }
+
+    public static boolean isAccessibilityServicePermissions(Context context, Class<? extends AccessibilityService> service) {
+        AccessibilityManager accessibilityManager = (AccessibilityManager) context.getSystemService(Context.ACCESSIBILITY_SERVICE);
+        List<AccessibilityServiceInfo> enabledServices = accessibilityManager.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_ALL_MASK);
+
+        for (AccessibilityServiceInfo enabledService : enabledServices) {
+            ServiceInfo serviceInfo = enabledService.getResolveInfo().serviceInfo;
+
+            if (serviceInfo.packageName.equals(context.getPackageName()) && serviceInfo.name.equals(service.getName()))
+                return true;
+        }
+        return false;
+    }
+
 }
--- app/src/main/java/com/stevenbenack/watchpass/WatcherService.java
+++ app/src/main/java/com/stevenbenack/watchpass/WatcherService.java
@@ -1,16 +1,53 @@
 package com.stevenbenack.watchpass;
 
 import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
 
 public class WatcherService extends AccessibilityService {
-    @Override
-    public void onAccessibilityEvent(AccessibilityEvent event) {
+    private static final String TAG = "CustomAccessibility";
 
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
+        AccessibilityNodeInfo source = accessibilityEvent.getSource();
+        if (source == null) {
+            return;
+        }
+        Log.d(TAG, "onAccessibilityEvent: getCollectionInfo " + source.getCollectionInfo());
+        Log.d(TAG, "onAccessibilityEvent: getLabeledBy " + source.getLabeledBy());
+        Log.d(TAG, "onAccessibilityEvent: getViewIdResourceName " + source.getViewIdResourceName());
+        Log.d(TAG, "onAccessibilityEvent: describeContents " + source.describeContents());
+        Log.d(TAG, "onAccessibilityEvent: getClassName " + source.getClassName());
+        Log.d(TAG, "onAccessibilityEvent: getContentDescription " + source.getContentDescription());
     }
+
 
     @Override
     public void onInterrupt() {
 
+    }
+
+    @Override
+    protected void onServiceConnected() {
+        Log.d(TAG, "onServiceConnected");
+        AccessibilityServiceInfo info = new AccessibilityServiceInfo();
+
+//        info.eventTypes = AccessibilityEvent.TYPE_WINDOWS_CHANGED | AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED | AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED;
+        info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
+
+        info.packageNames = new String[]{"com.stevenbenack.watchpass", "com.facebook.orca"};
+
+        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC;
+
+        info.flags = AccessibilityServiceInfo.DEFAULT;
+        info.flags = AccessibilityServiceInfo.FLAG_INCLUDE_NOT_IMPORTANT_VIEWS;
+        info.flags = AccessibilityServiceInfo.FLAG_REPORT_VIEW_IDS;
+        info.flags = AccessibilityServiceInfo.FLAG_REQUEST_ENHANCED_WEB_ACCESSIBILITY;
+        info.flags = AccessibilityServiceInfo.FLAG_RETRIEVE_INTERACTIVE_WINDOWS;
+
+        info.notificationTimeout = 0;
+        this.setServiceInfo(info);
     }
 }
